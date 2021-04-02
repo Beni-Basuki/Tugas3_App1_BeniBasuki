daea5f1471a3714f0e95bd31449c2c00adf3183b
Beni.Basuki <11950115025@students.uin-suska.ac.id>
2021-04-02 17:32:38 +0700

    first commit

---
+++ app/src/main/java/com/example/bapps/RSAwalBros.java
@@ -0,0 +1,62 @@
+package com.example.bapps;
+
+import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class RSAwalBros extends ListActivity {
+
+    protected void onCreate (Bundle icicle){
+        super.onCreate(icicle);
+        String[] listAct = new String[] {"Call Center",
+                "SMS Center", "Driving Direction", "Website", "Info Google", "Exit"};
+        this.setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,listAct));
+    }
+
+    protected void onListItemClick (ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+        Object o = this.getListAdapter().getItem(position);
+        String pilihan = o.toString();
+        tampilkanpilihan(pilihan);
+    }
+
+    private void tampilkanpilihan(String pilihan) {
+        try {
+            Intent a = null;
+            if (pilihan.equals("Call Center"))
+            {
+             String notelp ="tel:085397115771";
+             a = new Intent(Intent.ACTION_DIAL, Uri.parse(notelp));
+            } else if (pilihan.equals("SMS Center"))
+            {
+                String smsText = "Beni Basuki";
+                a = new Intent(Intent.ACTION_VIEW);
+                a.setData(Uri.parse("sms: 085397115771"));
+                a.putExtra("sms_body", smsText);
+            } else if (pilihan.equals("Driving Direction"))
+            {
+                String lokasirs = "google.navigation:q=0.463823,101.390353";
+                a = new Intent(Intent.ACTION_VIEW,Uri.parse(lokasirs));
+
+            } else if (pilihan.equals("Website"))
+            {
+                String website = "http://awalbros.com/branch/pekanbaru/";
+                a = new Intent(Intent.ACTION_VIEW,Uri.parse(website));
+
+            }else if (pilihan.equals("Info Google"))
+            {
+                a = new Intent(Intent.ACTION_WEB_SEARCH);
+                a.putExtra(SearchManager.QUERY,"Rumah Sakit Awal Bross");
+            }
+            startActivity(a);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
